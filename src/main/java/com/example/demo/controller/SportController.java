455ad1ced9dce7638901aecbfc59afbbaccd76b7
Krzyskoo <krzysztof_kandyba@o2.pl>
2025-05-10 00:31:41 +0200

    fix api

--- src/main/java/com/example/demo/controller/SportController.java
+++ src/main/java/com/example/demo/controller/SportController.java
@@ -1,44 +1,23 @@
 package com.example.demo.controller;
 
-import com.example.demo.constants.ApplicationConstants;
-import com.example.demo.dtos.OddsDTO;
-import com.example.demo.dtos.ScoreDTO;
-import com.example.demo.proxy.SportApiProxy;
 import com.example.demo.services.SportService;
-import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
 public class SportController {
-    private final SportApiProxy proxy;
-    private final Environment env;
     private final SportService sportService;
 
-    public SportController(SportApiProxy proxy, Environment env, SportService sportService) {
-        this.proxy = proxy;
-        this.env = env;
+    public SportController(SportService sportService) {
         this.sportService = sportService;
     }
 
     @GetMapping("/sports")
-    public Set<Object> getMessagesByStatus(){
-        return sportService.getSports();
-    }
-    @GetMapping("/event/odds/{sport}")
-    List<OddsDTO> getOdds(@PathVariable("sport") String sport,
-                          @RequestParam("regions") String regions,
-                          @RequestParam("markets") String markets){
-        return proxy.getOdds(sport, env.getProperty(ApplicationConstants.SPORT_API_KEY), regions,markets);
-    }
-
-    @GetMapping("/event/sport/{sport}/score")
-    List<ScoreDTO> getScore(@PathVariable("sport") String sport){
-        return proxy.getScore(sport, env.getProperty(ApplicationConstants.SPORT_API_KEY),1);
+    public ResponseEntity<Set<Object>> getMessagesByStatus(){
+        return ResponseEntity.status(HttpStatus.OK).body(sportService.getSports());
     }
 }
